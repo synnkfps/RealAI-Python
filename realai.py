6f6a68669d70939c6d6beaff87bf2ca4dba81841
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-23 16:08:02 -0300

    going good

--- realai.py
+++ realai.py
@@ -1,3 +1,7 @@
+from . import WordData as wd
+
+WordData = wd.WordData()
+
 WordDataSet = []
 Words = [] # string
 Frequecies = [] # ints
@@ -46,5 +50,80 @@ def getWords():
     		# newset.setFrequency(Frequencies.get(i));
             # getWordDataSet().add(newset);
             pass 
+        
+        file.close()
     except:
         print('Error at getting words')
+
+def savePreWords(word):
+    try:
+        file = open(f'Pre-{word}.txt', 'w')
+        WordsLine = ''
+        WordSet = len(getWordDataSet())
+
+        i = 0 
+        while i < len(getWordDataSet):
+            # WordsLine = getWordDataSet().get(i).getWord() + "~" + getWordDataSet().get(i).getFrequency().toString();
+            WordsLine = getWordDataSet()[i]+ '~' + WordData.getFrequency(getWordDataSet()[i])
+            WordSet[i] = WordsLine
+            file.write(WordsLine + '\n')
+            
+            i += 1 # i++
+        
+        file.close()
+    except:
+        print('Error at saving pre words')
+    
+
+def getProWords(word):
+    getWordDataSet().clear()
+    Words.clear()
+    Frequecies.clear()
+    WordSet = []
+    file = open(f'Pro-{word}.txt')
+
+    try:
+        line = ''
+        if not not file.readlines():
+            for line in file.readlines():
+                if '~' in line:
+                    WordSet = line.split('~')
+                    Words.append(WordSet[0])
+                    Frequecies.append(WordSet[1])
+            
+        for i in Words:
+            newset = WordData
+            newset.setWord(Words[i])
+            newset.setFrequency(Frequecies[i]) 
+            getWordDataSet().append(newset)
+        
+        file.close()
+    except:
+        print('Error on getting pro words')
+
+def saveInputList():
+    try:
+        file = open('InputList.txt', 'w')
+
+        for i in InputList:
+            file.write(InputList[i] + '\n')
+
+        file.close()
+    except:
+        print('Error on saving input list')
+
+def getInputList():
+    InputList.clear()
+    try:
+        file = open('InputList.txt','w')
+        if not not file.readlines():
+            for line in file.readlines():
+                if not line == '':
+                    InputList.append(line)
+
+        file.close()
+    except:
+        print('Error on getting input list')
+
+
+
