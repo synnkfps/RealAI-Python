316f9e9b787a6522a992b99c68d4c9993e35c09a
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-23 16:08:24 -0300

    Create WordData.py

---
+++ WordData.py
@@ -0,0 +1,15 @@
+class WordData():
+    Word = ''
+    Frequency = 0
+
+    def getWord(self):
+        return self.Word 
+    
+    def setWord(self, word):
+        self.Word = word
+    
+    def getFrequency(self):
+        return self.Frequency
+
+    def setFrequency(self, frequency):
+        self.Frequency = frequency
